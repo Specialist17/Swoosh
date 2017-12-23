a9b4eb2308eeaa87de53c4e0413a4faa531b7499
Specialist17 <arocho.fernando.13@gmail.com>
2017-12-22 22:45:13 -0400

    finished autolayout

---
+++ app-swoosh/app-swoosh/BorderButton.swift
@@ -0,0 +1,27 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Fernando on 12/22/17.
+//  Copyright © 2017 Specialist. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 3.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+
+}
