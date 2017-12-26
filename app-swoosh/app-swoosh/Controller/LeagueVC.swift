74d4982a3ffe194130e4984bd4ff9877648b5192
Specialist17 <arocho.fernando.13@gmail.com>
2017-12-26 15:07:19 -0400

    added player struct

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,12 +10,21 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        player = Player()
+        nextBtn.isEnabled = false
     }
 
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
     
     @IBAction func onNextTapped(_ sender: BorderButton) {
         
@@ -23,6 +32,24 @@ class LeagueVC: UIViewController {
         
     }
     
+    
+    @IBAction func onMensTapped(_ sender: BorderButton) {
+        selectLeague(leagueType: "mens")
+        
+    }
+    
+    @IBAction func onWomensTapped(_ sender: BorderButton) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoEdTapped(_ sender: BorderButton) {
+        selectLeague(leagueType: "co-ed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
     /*
     // MARK: - Navigation
 
